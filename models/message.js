fcbe0d78d0e748025fdfe4f79967ba23e605edf7
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2013-07-04 23:46:29 -0400

    Add index to message date

--- models/message.js
+++ models/message.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var messageSchema = new mongoose.Schema({
   userName: String,
   content: String,
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now, index: true }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
